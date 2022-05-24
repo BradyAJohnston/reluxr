08d5d884bd751f741de42418d63d6da727c4dd8d
Brady <brady.johnston@me.com>
2022-05-24 16:26:09 +0800

    clean up

--- better_parsing_of_bullshit_file.R
+++ better_parsing_of_bullshit_file.R
@@ -2,84 +2,78 @@ library(reluxr)
 library(tidyverse)
 
 fl <- "inst/newfiles/20220323_DeconvoPlate_384.csv"
-counter <- 0
-out_list <- list()
-
-last_line_details <- "End Kinetic"
-
-find_end_of_metadata <- function(file) {
-  max(which(readLines(file) %>% str_detect(last_line_details)))
-}
-end_met <- find_end_of_metadata(fl)
-raw_readings <- read_csv(fl, skip = end_met)
-
-df <-
-  raw_readings |>
-  fill(1) |>
-  mutate(
-    group = cumsum(is.na(...2))
-  ) |>
-  drop_na(2) |>
-  rename(signal = 1) |>
-  group_by(group, signal) |>
-  nest() |>
-  filter(signal != "Results") |>
-
-  mutate(
-    data = map(data, janitor::row_to_names, row_number = 1),
-    data = map(data, janitor::clean_names),
-    data = map(data, ~dplyr::rename_with(.x, .fn = ~if_else(str_detect(.x, "t_"), "temp", .x))),
-    data = map(data, tidyr::pivot_longer, cols = c(-1, -2), names_to = "well"),
-    # data = map(data, lubridate::time_length())
-    ) |>
-  unnest(data) |>
-  ungroup() |>
-  select(-group) |>
-  mutate(
-    id = str_extract(signal, "(?<=_)\\d"),
-    signal = str_extract(signal, ".+(?=_)")
-  ) |>
-  group_by(
-    signal, id
-  ) |>
-  # drop_na() #|>
-  mutate(
-    time = lubridate::hms(time),
-    value = as.numeric(value)
-  )
 
 
-df <-
-  df |>
-  group_by(signal, id) |>
-  nest() |>
-  mutate(
-    max_time = map_dbl(data, ~max(as.numeric(.x$time)))
-  ) |>
-  group_by(signal) |>
-  arrange(signal, id) |>
-  mutate(
-    time_adjust = lag(max_time),
-    time_adjust = if_else(is.na(time_adjust), 0, time_adjust)
-  ) |>
-  unnest(data) |>
-  mutate(
-    time = as.numeric(time) + time_adjust
-  ) |>
-    select(-max_time, -time_adjust) |>
-    mutate(
-      id = factor(id, labels = c("before", "after"))
+read_plate <- function(csv, skip = 0) {
+  last_line_details <- "End Kinetic"
+
+  find_end_of_metadata <- function(file) {
+    max(which(readLines(file) %>% stringr::str_detect(last_line_details)))
+  }
+  end_met <- find_end_of_metadata(fl)
+  raw_readings <- readr::read_csv(
+    file = fl,
+    name_repair = "unique",
+    skip = find_end_of_metadata(fl)
     )
 
+  df <-
+    raw_readings |>
+    tidyr::fill(1) |>
+    dplyr::mutate(group = cumsum(is.na(...2))) |>
+    tidyr::drop_na(2) |>
+    dplyr::rename(signal = 1) |>
+    dplyr::group_by(group, signal) |>
+    tidyr::nest() |>
+    dplyr::filter(signal != "Results") |>
+
+    dplyr::mutate(
+      data = map(data, janitor::row_to_names, row_number = 1),
+      data = map(data, janitor::clean_names),
+      data = map(data, ~ dplyr::rename_with(
+        .x, .fn = ~ dplyr::if_else(str_detect(.x, "t_"), "temp", .x)
+      )),
+      data = map(data, tidyr::pivot_longer, cols = c(-1, -2), names_to = "well")
+    ) |>
+    tidyr::unnest(data) |>
+    dplyr::ungroup() |>
+    dplyr::select(-group) |>
+    dplyr::mutate(id = stringr::str_extract(signal, "(?<=_)\\d"),
+           signal = stringr::str_extract(signal, ".+(?=_)")) |>
+    dplyr::group_by(signal, id) |>
+    # drop_na() #|>
+    dplyr::mutate(time = lubridate::hms(time),
+           value = as.numeric(value))
+
+
+  df <-
+    df |>
+    dplyr::group_by(signal, id) |>
+    tidyr::nest() |>
+    dplyr::mutate(max_time = map_dbl(data, ~ max(as.numeric(.x$time)))) |>
+    dplyr::group_by(signal) |>
+    dplyr::arrange(signal, id) |>
+    dplyr::mutate(
+      time_adjust = dplyr::lag(max_time),
+      time_adjust = dplyr::if_else(is.na(time_adjust), 0, time_adjust)
+    ) |>
+    tidyr::unnest(data) |>
+    dplyr::mutate(time = as.numeric(time) + time_adjust) |>
+    dplyr::select(-max_time, -time_adjust) |>
+    dplyr::mutate(id = forcats::fct_relabel(
+      .f = id,
+      .fun = ~paste0("plate_", .x))
+      )
+  df
+}
+
+df <- read_plate(fl)
+
 df  |>
-  ggplot(aes(as.numeric(time)/ 60 / 60, value, group = well)) +
+  ggplot(aes(as.numeric(time) / 60 / 60, value, group = well)) +
   geom_line(aes(colour = id)) +
-  facet_grid(
-    # cols = vars(id),
+  # geom_point(aes(colour = id), alpha = 0.3) +
+  facet_grid(# cols = vars(id),
     rows = vars(signal),
-    scales = "free"
-  ) +
+    scales = "free") +
   scale_y_log10()
-
-
-
