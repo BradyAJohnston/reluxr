3758974cc2d031155e395f1ec95f643eed342ec9
Brady <brady.johnston@me.com>
2022-05-27 16:39:13 +0800

    remove old code, big cleanup

--- re-write-testing.R
+++ re-write-testing.R
@@ -41,3 +41,36 @@ rl_decon_frames(mat, D_best) |>
   geom_line() +
   scale_y_log10()
 
+library(wellr)
+target_wells <- c(well_join(1:8, 5), well_join(1:8, 7), well_join(c(2, 7), 6))
+
+fl <- "inst/Xfiles/tecan/tecanON1.xlsx"
+
+df <- readxl::read_excel(fl, skip = 168, n_max = 122) |>
+  janitor::clean_names()
+
+df2 <- df |>
+  pipebind::bind(x, tidyr::pivot_longer(
+    data = x,
+    cols = which(wellr:::well_check(colnames(x))),
+    names_to = "well",
+    values_to = "fluoro"
+  )) |>
+  dplyr::mutate(
+    well = wellr::well_format(well)
+  )
+
+df2 |>
+  wellr::well_df_to_mat_frames("fluoro", "time_s") |>
+  rl_decon_frames(D_best) |>
+  as_tibble() |>
+  mutate(
+    frames = row_number()
+  ) |>
+  pivot_longer(-frames) |>
+  ggplot(aes(frames, value, group = name, colour = name %in% target_wells)) +
+  geom_line() +
+  scale_y_log10(limits = c(0.1, NA)) +
+  scale_x_continuous(expand = expansion()) +
+  labs(colour = "Sample Well") +
+  theme_light()
